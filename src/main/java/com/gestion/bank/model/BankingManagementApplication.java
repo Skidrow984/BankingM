a09a0811c8f6382b4bbf758292a8222391650d7e
Hamza Samidine <67514436+Skidrow984@users.noreply.github.com>
2021-05-20 14:45:25 +0100

    Add files via upload

---
+++ src/main/java/com/gestion/bank/model/BankingManagementApplication.java
@@ -0,0 +1,14 @@
+package com.gestion.bank.model;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingManagementApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(BankingManagementApplication.class, args);
+	}
+
+}
